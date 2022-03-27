2fd1153530245807fa98f6e832bf99d6509d2ab0
shilpa170691 <shilpabbhadran@gmail.com>
2022-03-27 17:29:01 +0530

    main  project added to master branch

---
+++ src/arithmeticoperations/Add.java
@@ -0,0 +1,24 @@
+package arithmeticoperations;
+
+public class Add {
+
+	public static void main(String[] args) {
+		
+		
+		int a=45;
+		
+		int b=45;
+		System.out.println(a+b);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
